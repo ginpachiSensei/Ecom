8b5d1158041c0c3560e95a1b999e03f56b822cdd
satyam <satyamrocker1234@gmail.com>
2021-10-11 02:49:20 +0530

    added product screen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { LinkContainer } from 'react-router-bootstrap';
 
 function Header() {
