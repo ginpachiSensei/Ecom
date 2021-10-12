19bc64b0e6b6ecc27c3ca220d433d82c23a016f8
satyam <satyamrocker1234@gmail.com>
2021-10-12 22:19:31 +0530

    fetch product from backend

---
+++ backend/data/users.js
@@ -0,0 +1,23 @@
+//TODO think of user data and password hashing 
+import bcrypt from 'bcryptjs';
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456',10),
+        isAdmin: true,
+    },
+    {
+        name: 'satyam',
+        email: 'satyam@example.com',
+        password: bcrypt.hashSync('123456',10),
+    },
+    {
+        name: 'shivam',
+        email: 'shivam@example.com',
+        password: bcrypt.hashSync('123456',10),
+    },
+]
+
+export default users
