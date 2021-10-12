4f6d2ae0ea226a7b30f05ad38909e60c61c34e0c
satyam <satyamrocker1234@gmail.com>
2021-10-12 15:04:09 +0530

    fetching products from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,31 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-import products from "../products";
-import Product from "../components/Product";
+import React, { useState, useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import axios from 'axios'
 
-function HomeScreen() {
+const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async () => {
+    const res = await axios.get('/api/products')
+    setProducts(res.data)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return (
     <>
-      <h1>Latest products</h1>
+      <h1>Latest Products</h1>
       <Row>
-        {products.map((p) => (
-          <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={p} />
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
     </>
-  );
+  )
 }
-
-export default HomeScreen;
+export default HomeScreen
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import { Link, useLocation, useParams } from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios';
 
 const ProductScreen = () => {
     const params = useParams();
-    const product = products.find((p) => p._id === params.id)
+    // const product = products.find((p) => p._id === params.id)
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+      const fetchProduct = async () => {
+        const { data } = await axios.get(`/api/products/${params.id}`)
+  
+        setProduct(data)
+      }
+  
+      fetchProduct()
+       // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
