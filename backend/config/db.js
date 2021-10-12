70bc55e05251576b8c61e64c9bad20a625810294
satyam <satyamrocker1234@gmail.com>
2021-10-12 19:45:58 +0530

    create db-model

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connectDB = async ()=> {
+    try {
+        const conn = await mongoose.connect(process.env.Mongo_URI)
+        console.log(`mongoDB connected: ${conn.connection.host}`)
+    } catch (error) {
+         console.error(`ERROR: ${error.message}`)
+         process.exit(1)       
+    }
+}
+
+export default connectDB
---
+++ backend/db-model/review-model.js
@@ -0,0 +1,15 @@
+//TODO: add reviewSchema to product-model
+import mongoose from 'mongoose'
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default reviewSchema
