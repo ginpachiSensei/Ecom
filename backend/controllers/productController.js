39103b4c4bfbe567dbb5220b855d79a28d828bd8
satyam <satyamrocker1234@gmail.com>
2021-10-31 22:52:20 +0530

    added createProduct and updateProduct route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,41 +1,92 @@
-import asyncHandler from 'express-async-handler'
-import Product from '../db-models/productModel.js'
+import asyncHandler from "express-async-handler";
+import Product from "../db-models/productModel.js";
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+  const products = await Product.find({});
 
-  res.json(products)
-})
+  res.json(products);
+});
 
 // @desc    Fetch single product
 // @route   GET /api/products/:id
 // @access  Public
 const getProductById = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
 
   if (product) {
-    res.json(product)
+    res.json(product);
   } else {
-    res.status(404)
-    throw new Error('Product not found')
+    res.status(404);
+    throw new Error("Product not found");
   }
-})
+});
 
 // @desc    Delete a product
 // @route   DELETE /api/products/:id
 // @access  Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id)
-  if(product){
-    await product.remove()
-    res.json({message: 'product removed'})
-  }else{
-    res.status(404)
-    throw new Error('product not found')
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove();
+    res.json({ message: "product removed" });
+  } else {
+    res.status(404);
+    throw new Error("product not found");
   }
+});
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
 })
 
-export { getProducts, getProductById, deleteProduct }
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    // product.name = req.body.name || product.name
+    // product.price = req.body.price || product.price
+    // product.discription = req.body.discription || product.discription
+    // product.image = req.body.image || product.image
+    // product.brand = req.body.brand || product.brand
+    // product.category = req.body.category || product.category
+    // product.countInStock = req.body.countInStock || product.countInStock
+    product.name = req.body.name;
+    product.price = req.body.price;
+    product.discription = req.body.discription;
+    product.image = req.body.image;
+    product.brand = req.body.brand;
+    product.category = req.body.category;
+    product.countInStock = req.body.countInStock;
+
+    const updatedProduct = await product.save();
+
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,19 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 import {
-    getProducts,
-    getProductById,
-    deleteProduct,
-  } from '../controllers/productController.js'
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+} from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware";
 
-router.route('/').get(getProducts)
-router.route('/:id').get(getProductById).delete(deleteProduct)
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
-export default router
+export default router;
