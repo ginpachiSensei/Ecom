df3d9d868a4a86e4e23c3014e96e9223ab2ffb29
satyam <satyamrocker1234@gmail.com>
2021-11-01 06:26:44 +0530

    added image upload

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -20,6 +22,7 @@ app.get('/' , (req,res) =>{
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 //TODO do something about payment gateway
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
@@ -27,6 +30,8 @@ app.get('/api/config/paypal', (req, res) =>
 
 app.use(notFound)
 app.use(errorHandler)
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 const PORT = process.env.PORT || 5000
 
