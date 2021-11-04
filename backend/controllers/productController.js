d101ad1e8ed2eb58058355c59f66374d244374e1
satyam <satyamrocker1234@gmail.com>
2021-11-04 17:56:40 +0530

    added review endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -65,13 +65,6 @@ const updateProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (product) {
-    // product.name = req.body.name || product.name
-    // product.price = req.body.price || product.price
-    // product.discription = req.body.discription || product.discription
-    // product.image = req.body.image || product.image
-    // product.brand = req.body.brand || product.brand
-    // product.category = req.body.category || product.category
-    // product.countInStock = req.body.countInStock || product.countInStock
     product.name = req.body.name;
     product.price = req.body.price;
     product.discription = req.body.discription;
@@ -89,4 +82,45 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct, createProduct, updateProduct };
+// @desc    Create new review
+// @route   POST /api/products/:id/reviews
+// @access  Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    )
+
+    if (alreadyReviewed) {
+      res.status(400)
+      throw new Error('Product already reviewed')
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    }
+
+    product.reviews.push(review)
+
+    product.numReviews = product.reviews.length
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length
+
+    await product.save()
+    res.status(201).json({ message: 'Review added' })
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct,createProductReview };
