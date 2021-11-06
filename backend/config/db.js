679e4d442b35ec476addd54d6427bbeb50c03e3f
satyam <satyamrocker1234@gmail.com>
2021-11-06 20:19:25 +0530

    edited db file

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async ()=> {
     try {
-        const conn = await mongoose.connect(process.env.Mongo_URI)
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`mongoDB connected: ${conn.connection.host}`)
     } catch (error) {
          console.error(`ERROR: ${error.message}`)
